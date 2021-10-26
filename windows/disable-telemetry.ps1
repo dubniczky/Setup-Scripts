d5deb6183cad0938526039ab330bc208754da288
Richard Nagy <nagy.richard.antal@gmail.com>
2021-10-26 20:26:59 +0200

    Added Windows setup and configuration tools

---
+++ windows/disable-telemetry.ps1
@@ -0,0 +1,364 @@
+# Disable windows global telemetry and tracking
+#Requires -RunAsAdministrator
+
+###
+### Disable Tememetry
+###
+
+
+Write-Output "Disabling Telemetry..."
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Policies\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name "AllowTelemetry" -Type DWord -Value 0
+If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PreviewBuilds"))
+{
+    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PreviewBuilds" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\PreviewBuilds" -Name "AllowBuildPreview" -Type DWord -Value 0
+If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\CurrentVersion\Software Protection Platform"))
+{
+    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\CurrentVersion\Software Protection Platform" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\CurrentVersion\Software Protection Platform" -Name "NoGenTicket" -Type DWord -Value 1
+If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\SQMClient\Windows"))
+{
+    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\SQMClient\Windows" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\SQMClient\Windows" -Name "CEIPEnable" -Type DWord -Value 0
+If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\AppCompat"))
+{
+    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\AppCompat" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\AppCompat" -Name "AITEnable" -Type DWord -Value 0
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\AppCompat" -Name "DisableInventory" -Type DWord -Value 1
+If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\AppV\CEIP"))
+{
+    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\AppV\CEIP" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\AppV\CEIP" -Name "CEIPEnable" -Type DWord -Value 0
+If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\TabletPC"))
+{
+    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\TabletPC" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\TabletPC" -Name "PreventHandwritingDataSharing" -Type DWord -Value 1
+If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\TextInput"))
+{
+    New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\TextInput" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\TextInput" -Name "AllowLinguisticDataCollection" -Type DWord -Value 0
+Disable-ScheduledTask -TaskName "Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" | Out-Null
+Disable-ScheduledTask -TaskName "Microsoft\Windows\Application Experience\ProgramDataUpdater" | Out-Null
+Disable-ScheduledTask -TaskName "Microsoft\Windows\Autochk\Proxy" | Out-Null
+Disable-ScheduledTask -TaskName "Microsoft\Windows\Customer Experience Improvement Program\Consolidator" | Out-Null
+Disable-ScheduledTask -TaskName "Microsoft\Windows\Customer Experience Improvement Program\UsbCeip" | Out-Null
+Disable-ScheduledTask -TaskName "Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" | Out-Null
+Disable-ScheduledTask -TaskName "Microsoft\Office\Office ClickToRun Service Monitor" -ErrorAction SilentlyContinue | Out-Null
+Disable-ScheduledTask -TaskName "Microsoft\Office\OfficeTelemetryAgentFallBack2016" -ErrorAction SilentlyContinue | Out-Null
+Disable-ScheduledTask -TaskName "Microsoft\Office\OfficeTelemetryAgentLogOn2016" -ErrorAction SilentlyContinue | Out-Null
+
+
+###
+### Disable Cortana
+###
+
+
+Write-Output "Disabling Cortana..."
+If (!(Test-Path "HKCU:\Software\Microsoft\Personalization\Settings"))
+{
+    New-Item -Path "HKCU:\Software\Microsoft\Personalization\Settings" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Personalization\Settings" -Name "AcceptedPrivacyPolicy" -Type DWord -Value 0
+If (!(Test-Path "HKCU:\Software\Microsoft\InputPersonalization\TrainedDataStore"))
+{
+    New-Item -Path "HKCU:\Software\Microsoft\InputPersonalization\TrainedDataStore" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\InputPersonalization" -Name "RestrictImplicitTextCollection" -Type DWord -Value 1
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\InputPersonalization" -Name "RestrictImplicitInkCollection" -Type DWord -Value 1
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\InputPersonalization\TrainedDataStore" -Name "HarvestContacts" -Type DWord -Value 0
+Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowCortanaButton" -Type DWord -Value 0
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Experience\AllowCortana" -Name "Value" -Type DWord -Value 0
+If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"))
+{
+    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "AllowCortana" -Type DWord -Value 0
+If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\InputPersonalization"))
+{
+    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\InputPersonalization" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\InputPersonalization" -Name "AllowInputPersonalization" -Type DWord -Value 0
+Get-AppxPackage "Microsoft.549981C3F5F10" | Remove-AppxPackage
+
+
+###
+### Disable Wi-Fi Sense
+###
+
+
+Write-Output "Disabling Wi-Fi Sense..."
+If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting"))
+{
+    New-Item -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\AllowWiFiHotSpotReporting" -Name "Value" -Type DWord -Value 0
+If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\AllowAutoConnectToWiFiSenseHotspots"))
+{
+    New-Item -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\AllowAutoConnectToWiFiSenseHotspots" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\PolicyManager\default\WiFi\AllowAutoConnectToWiFiSenseHotspots" -Name "Value" -Type DWord -Value 0
+If (!(Test-Path "HKLM:\SOFTWARE\Microsoft\WcmSvc\wifinetworkmanager\config"))
+{
+    New-Item -Path "HKLM:\SOFTWARE\Microsoft\WcmSvc\wifinetworkmanager\config" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\WcmSvc\wifinetworkmanager\config" -Name "AutoConnectAllowedOEM" -Type DWord -Value 0
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\WcmSvc\wifinetworkmanager\config" -Name "WiFISenseAllowed" -Type DWord -Value 0
+
+
+###
+### Disable SmartScreen
+###
+
+
+Write-Output "Disabling SmartScreen..."
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name "EnableSmartScreen" -Type DWord -Value 0
+If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\MicrosoftEdge\PhishingFilter"))
+{
+    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\MicrosoftEdge\PhishingFilter" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\MicrosoftEdge\PhishingFilter" -Name "EnabledV9" -Type DWord -Value 0
+
+
+###
+### Disable Bing Search in Start
+###
+
+
+Write-Output "Disabling Bing Search in Start Menu..."
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -Type DWord -Value 0
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "CortanaConsent" -Type DWord -Value 0
+If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"))
+{
+    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "DisableWebSearch" -Type DWord -Value 1
+
+
+###
+### Disable App Suggestions
+###
+
+
+Write-Output "Disabling Application suggestions..."
+Push-Location
+Set-Location HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager
+Set-ItemProperty -Path . -Name "ContentDeliveryAllowed" -Type DWord -Value 0
+Set-ItemProperty -Path . -Name "OemPreInstalledAppsEnabled" -Type DWord -Value 0
+Set-ItemProperty -Path . -Name "PreInstalledAppsEnabled" -Type DWord -Value 0
+Set-ItemProperty -Path . -Name "PreInstalledAppsEverEnabled" -Type DWord -Value 0
+Set-ItemProperty -Path . -Name "SilentInstalledAppsEnabled" -Type DWord -Value 0
+Set-ItemProperty -Path . -Name "SubscribedContent-310093Enabled" -Type DWord -Value 0
+Set-ItemProperty -Path . -Name "SubscribedContent-314559Enabled" -Type DWord -Value 0
+Set-ItemProperty -Path . -Name "SubscribedContent-338387Enabled" -Type DWord -Value 0
+Set-ItemProperty -Path . -Name "SubscribedContent-338388Enabled" -Type DWord -Value 0
+Set-ItemProperty -Path . -Name "SubscribedContent-338389Enabled" -Type DWord -Value 0
+Set-ItemProperty -Path . -Name "SubscribedContent-338393Enabled" -Type DWord -Value 0
+Set-ItemProperty -Path . -Name "SubscribedContent-353694Enabled" -Type DWord -Value 0
+Set-ItemProperty -Path . -Name "SubscribedContent-353696Enabled" -Type DWord -Value 0
+Set-ItemProperty -Path . -Name "SubscribedContent-353698Enabled" -Type DWord -Value 0
+Set-ItemProperty -Path . -Name "SystemPaneSuggestionsEnabled" -Type DWord -Value 0
+Pop-Location
+If (!(Test-Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\UserProfileEngagement"))
+{
+    New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\UserProfileEngagement" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\UserProfileEngagement" -Name "ScoobeSystemSettingEnabled" -Type DWord -Value 0
+If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\WindowsInkWorkspace"))
+{
+    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\WindowsInkWorkspace" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\WindowsInkWorkspace" -Name "AllowSuggestedAppsInWindowsInkWorkspace" -Type DWord -Value 0
+# Empty placeholder tile collection in registry cache and restart Start Menu process to reload the cache
+If ([System.Environment]::OSVersion.Version.Build -ge 17134)
+{
+    $key = Get-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\CloudStore\Store\Cache\DefaultAccount\*windows.data.placeholdertilecollection\Current"
+    Set-ItemProperty -Path $key.PSPath -Name "Data" -Type Binary -Value $key.Data[0..15]
+    Stop-Process -Name "ShellExperienceHost" -Force -ErrorAction SilentlyContinue
+}
+
+
+###
+### Disable Activity History
+###
+
+
+Write-Output "Disabling Activity History..."
+Push-Location
+Set-Location HKLM:\SOFTWARE\Policies\Microsoft\Windows\System
+Set-ItemProperty -Path . -Name "EnableActivityFeed" -Type DWord -Value 0
+Set-ItemProperty -Path . -Name "PublishUserActivities" -Type DWord -Value 0
+Set-ItemProperty -Path . -Name "UploadUserActivities" -Type DWord -Value 0
+Pop-Location
+
+
+###
+### Disable Sensors and Location
+###
+
+
+Write-Output "Disabling sensors and location..."
+Push-Location
+Set-Location HKLM:\SOFTWARE\Policies\Microsoft\Windows\LocationAndSensors
+If (!(Test-Path .))
+{
+    New-Item -Path . -Force | Out-Null
+}
+Set-ItemProperty -Path . -Name "DisableSensors" -Type DWord -Value 1
+Set-ItemProperty -Path . -Name "DisableLocation" -Type DWord -Value 1
+Set-ItemProperty -Path . -Name "DisableLocationScripting" -Type DWord -Value 1
+Pop-Location
+
+
+###
+### Disable Maps
+###
+
+
+Write-Output "Disabling map auto updates..."
+Set-ItemProperty -Path "HKLM:\SYSTEM\Maps" -Name "AutoUpdateEnabled" -Type DWord -Value 0
+
+
+###
+### Disable Feedback
+###
+
+
+Write-Output "Disabling Feedback..."
+If (!(Test-Path "HKCU:\Software\Microsoft\Siuf\Rules"))
+{
+    New-Item -Path "HKCU:\Software\Microsoft\Siuf\Rules" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Siuf\Rules" -Name "NumberOfSIUFInPeriod" -Type DWord -Value 0
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name "DoNotShowFeedbackNotifications" -Type DWord -Value 1
+Disable-ScheduledTask -TaskName "Microsoft\Windows\Feedback\Siuf\DmClient" -ErrorAction SilentlyContinue | Out-Null
+Disable-ScheduledTask -TaskName "Microsoft\Windows\Feedback\Siuf\DmClientOnScenarioDownload" -ErrorAction SilentlyContinue | Out-Null
+
+
+###
+### Disable Tailored Experience Diagnostics
+###
+
+
+Write-Output "Disabling Tailored Experience diagnostics..."\
+Push-Location
+Set-Location HKCU:\Software\Policies\Microsoft\Windows\CloudContent
+If (!(Test-Path .))
+{
+    New-Item -Path . -Force | Out-Null
+}
+Set-ItemProperty -Path . -Name "DisableTailoredExperiencesWithDiagnosticData" -Type DWord -Value 1
+Pop-Location
+
+
+###
+### Disable Advertising ID
+###
+
+
+Write-Output "Disabling Advertising ID..."
+Push-Location
+Set-Location HKLM:\SOFTWARE\Policies\Microsoft\Windows\AdvertisingInfo
+If (!(Test-Path .))
+{
+    New-Item -Path . | Out-Null
+}
+Set-ItemProperty -Path . -Name "DisabledByGroupPolicy" -Type DWord -Value 1
+
+
+###
+### Disable Error Reporting
+###
+
+
+Write-Output "Disabling Error reporting..."
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\Windows Error Reporting" -Name "Disabled" -Type DWord -Value 1
+Disable-ScheduledTask -TaskName "Microsoft\Windows\Windows Error Reporting\QueueReporting" | Out-Null
+
+
+###
+### Disable Telemetry Services
+###
+
+
+Write-Output "Disabling Telemetry Services..."
+Stop-Service "DiagTrack" -WarningAction SilentlyContinue
+Set-Service "DiagTrack" -StartupType Disabled
+
+
+###
+### Disable UWP app background
+###
+
+
+Write-Output "Disabling UWP apps background access..."
+If ([System.Environment]::OSVersion.Version.Build -ge 17763)
+{
+    If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\AppPrivacy"))
+    {
+        New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\AppPrivacy" -Force | Out-Null
+    }
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\AppPrivacy" -Name "LetAppsRunInBackground" -Type DWord -Value 2
+}
+Else
+{
+    Get-ChildItem -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications" -Exclude "Microsoft.Windows.Cortana*", "Microsoft.Windows.ShellExperienceHost*" | ForEach-Object
+    {
+        Set-ItemProperty -Path $_.PsPath -Name "Disabled" -Type DWord -Value 1
+        Set-ItemProperty -Path $_.PsPath -Name "DisabledByUser" -Type DWord -Value 1
+    }
+}
+
+
+###
+### Disable Windows Defender
+###
+
+
+Write-Output "Disabling Windows Defender..."
+If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender"))
+{
+    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "DisableAntiSpyware" -Type DWord -Value 1
+If ([System.Environment]::OSVersion.Version.Build -ge 15063)
+{
+    Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "SecurityHealth" -ErrorAction SilentlyContinue
+}
+If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet"))
+{
+    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet" -Force | Out-Null
+}
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet" -Name "SpynetReporting" -Type DWord -Value 0
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet" -Name "SubmitSamplesConsent" -Type DWord -Value 2
+
+
+
+###
+### Disable Download Block
+###
+
+Write-Output "Disabling blocking of downloaded files..."
+If (!(Test-Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Attachments"))
+{
+    New-Item -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Attachments" | Out-Null
+}
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Attachments" -Name "SaveZoneInformation" -Type DWord -Value 1
+
+
+###
+### Disable Remote Assistance
+###
+
+
+Write-Output "Disabling Remote Assistance..."
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Remote Assistance" -Name "fAllowToGetHelp" -Type DWord -Value 0
+Get-WindowsCapability -Online | Where-Object { $_.Name -like "App.Support.QuickAssist*" } | Remove-WindowsCapability -Online | Out-Null
---
+++ windows/setup-packages.ps1
@@ -0,0 +1,36 @@
+# General
+winget install 7zip.7zip
+winget install AMD.RyzenMaster
+winget install QL-Win.QuickLook
+winget install Corsair.iCUE
+winget install Opera.OperaGX
+winget install Oracle.JavaRuntimeEnvironment
+winget install Microsoft.PowerToys
+winget install TeamViewer.TeamViewer
+winget install Notion.Notion
+winget install OpenWhisperSystems.Signal
+winget install Discord.Discord
+
+
+# Dev
+winget install Git.Git
+winget install Google.Chrome
+winget install OpenJS.NodeJS
+winget install Docker.DockerDesktop
+winget install OpenVPNTechnologies.OpenVPN
+winget install Python.Python.3
+winget install Microsoft.WindowsTerminal
+winget install MongoDB.Server
+winget install MongoDB.Compass.Community
+winget install 9N0866FS04W8 # Dolby Access
+winget install Oracle.VirtualBox
+winget install TimKosse.FileZillaClient
+
+
+# Media & Gaming
+winget install FlawlessWidescreen.FlawlessWidescreen
+winget install clsid2.mpc-hc # Media player classic
+winget install 9WZDNCRFJ3TJ # Netflix
+winget install Deezer.Deezer
+winget install Valve.Steam
+winget install VideoLAN.VLC
