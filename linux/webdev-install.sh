bb62257125d5f3327fce3785d24e57041c0fc504
Richard Nagy <nagy.richard.antal@gmail.com>
2021-12-12 23:56:20 +0100

    Extracted git install from webdev-install

--- linux/webdev-install.sh
+++ linux/webdev-install.sh
@@ -16,14 +16,6 @@ sudo apt-get install curl -y
 sudo apt-get install unzip -y
 sudo apt-get install python3 -y
 
-# Install Git
-sudo apt-get install git -y
-git --version
-git config --global user.name "Richard Nagy"
-git config --global user.email "nagy.richard.antal@gmail.com"
-git config --global credential.helper store
-git config --list
-
 # Install Node
 sudo apt-get install nodejs -y
 sudo apt-get install nodemon -y
