ef6719bf39e9027b7fedad3e5325dcdcb7241f43
Richard Nagy <nagy.richard.antal@gmail.com>
2021-12-13 00:09:13 +0100

    Added node install script

---
+++ linux/install-node.sh
@@ -0,0 +1,18 @@
+# Install packages
+sudo apt-get install -y nodejs
+sudo apt-get install npm -y
+
+# Check versions
+node -v || nodejs -v
+npm -v
+
+# Install npm packages
+npm i -g nodemon           # Realtime dev
+npm i -g yarn              # Alternate PM
+npm i -g npm-check         # Search old/outdated packages
+npm i -g http-server       # Quick serve directory
+npm i -g tinypng-cli       # Quick serve directory
+npm i -g sort-package-json # Clean up package.json
+
+# List all global npm packages
+npm ls -g --depth=0
