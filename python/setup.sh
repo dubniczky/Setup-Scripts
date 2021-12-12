7204a5767561a6d2bd19d2747320e4fc43cb6299
Richard Nagy <nagy.richard.antal@gmail.com>
2021-12-13 00:27:02 +0100

    Added python setup script

---
+++ python/setup.sh
@@ -0,0 +1,10 @@
+# Environment
+pip install virtualenv
+pip install doctest
+
+# Math / Data Science
+pip install sklearn
+pip install seaborn
+pip install pandas
+pip install numpy
+pip install matplotlib
