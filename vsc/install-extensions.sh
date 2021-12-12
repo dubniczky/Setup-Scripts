752d4b9176b7eb312d932a13e679f97f9dfa4854
Richard Nagy <nagy.richard.antal@gmail.com>
2021-12-13 00:59:29 +0100

    Updated vsc extension installer

--- vsc/install-extensions.sh
+++ vsc/install-extensions.sh
@@ -1,19 +1,36 @@
-# Windows or Linux
+# Install VSC extensions
 
-code --install-extension bmewburn.vscode-intelephense-client
-code --install-extension DigitalBrainstem.javascript-ejs-support
-code --install-extension formulahendry.auto-rename-tag
-code --install-extension Gruntfuggly.todo-tree
-code --install-extension humao.rest-client
-code --install-extension laktak.hjson
-code --install-extension Lorenzo-Lupetti.easy-timer
-code --install-extension ms-azuretools.vscode-docker
+# General
+code --install-extension Gruntfuggly.todo-tree # Todo tree
+code --install-extension vscode-icons-team.vscode-icons # Explorer icons
+code --install-extension Lorenzo-Lupetti.easy-timer # Work timer
+code --install-extension ms-vscode.powershell # Powershell
+code --install-extension shd101wyy.markdown-preview-enhanced # .md preview
+code --install-extension yzhang.markdown-all-in-one # .md env
+
+# Web Client
+code --install-extension octref.vetur # Vue
+code --install-extension formulahendry.auto-rename-tag # HTML tag rename
+
+# Server
+code --install-extension ritwickdey.LiveServer # Live server
+code --install-extension DigitalBrainstem.javascript-ejs-support # .ejs
+code --install-extension humao.rest-client # HTTP requests
+code --install-extension ms-azuretools.vscode-docker # Docker
+
+# Security
+code --install-extension judge2020.duckyscript
+
+# Typescript
+code --install-extension ms-vscode.vscode-typescript-next
+
+# Python
+code --install-extension batisteo.vscode-django
+code --install-extension donjayamanne.python-environment-manager
+code --install-extension donjayamanne.python-extension-pack
 code --install-extension ms-python.python
-code --install-extension ms-python.vscode-pylance
 code --install-extension ms-toolsai.jupyter
 code --install-extension ms-toolsai.jupyter-keymap
 code --install-extension ms-toolsai.jupyter-renderers
-code --install-extension ms-vscode.powershell
-code --install-extension octref.vetur
-code --install-extension ritwickdey.LiveServer
-code --install-extension vscode-icons-team.vscode-icons
+code --install-extension ms-python.vscode-pylance
+code --install-extension njpwerner.autodocstring
