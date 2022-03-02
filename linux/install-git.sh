9d819972288735da50f353bc469ab751a06dbc32
Richard Nagy <nagy.richard.antal@gmail.com>
2022-03-02 11:37:20 +0000

    Added autoclrf

--- linux/install-git.sh
+++ linux/install-git.sh
@@ -4,4 +4,5 @@ git --version
 git config --global user.name "Richard Nagy"
 git config --global user.email "nagy.richard.antal@gmail.com"
 git config --global credential.helper store
+git config --global core.autocrlf true
 git config --list
