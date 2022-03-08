c8ff66677d4862ef8502aded97fbd3613acafeb2
Richard Nagy <nagy.richard.antal@gmail.com>
2022-03-08 19:26:23 +0000

    Added loading venv to aliases

--- python/aliases.sh
+++ python/aliases.sh
@@ -9,6 +9,7 @@ alias pym="python manage.py"
 
 # Venv
 alias pyv.init="python -m venv .venv"
+alias pyv.load="source .venv/bin/activate"
 alias pyv.save="pip freeze > ./requirements.txt"
-alias pyv.load="pip install -r ./requirements.txt"
+alias pyv.inst="pip install -r ./requirements.txt"
 alias pyv.exit="deactivate"
