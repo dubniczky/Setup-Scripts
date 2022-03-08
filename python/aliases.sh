e4f071a1d5f756fdd557254e443e46c007670231
Richard Nagy <nagy.richard.antal@gmail.com>
2022-03-08 17:04:48 +0000

    Added python package install alias

--- python/aliases.sh
+++ python/aliases.sh
@@ -10,4 +10,5 @@ alias pym="python manage.py"
 # Venv
 alias pyv.init="python -m venv .venv"
 alias pyv.save="pip freeze > ./requirements.txt"
+alias pyv.load="pip install -r ./requirements.txt"
 alias pyv.exit="deactivate"
