3734bd1db9d13e334411710b8e08c6bcf6231f0e
Richard Nagy <nagy.richard.antal@gmail.com>
2022-04-04 22:26:11 +0200

    Moved git install to git global-config script

--- git/global-config.sh
+++ git/global-config.sh
@@ -1,6 +1,3 @@
-sudo apt-get update
-sudo apt-get install git -y
-git --version
 git config --global user.name "Richard Nagy"
 git config --global user.email "nagy.richard.antal@gmail.com"
 git config --global credential.helper store
