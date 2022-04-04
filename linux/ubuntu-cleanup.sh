63d6663d6f128ec78d7c8fdbbec786262afa0199
Richard Nagy <nagy.richard.antal@gmail.com>
2022-04-04 22:53:53 +0200

    Added items to ubuntu cleanup

--- linux/ubuntu-cleanup.sh
+++ linux/ubuntu-cleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Uninstall Games
+echo "Uninstalling games..."
 sudo apt purge -y aisleriot \
     gnome-sudoku \
     ace-of-penguins \
@@ -13,5 +14,16 @@ sudo apt purge -y aisleriot \
     gnome-mahjongg \
     gnome-mines
 
+# Remove old kernels
+echo "Removing old kernels..."
+sudo dpkg -l linux-{image,headers}-* | awk '/^ii/{print $2}' | egrep '[0-9]+\.[0-9]+\.[0-9]+' | grep -v $(uname -r) | xargs sudo apt-get -y purge
+
+# Remove error reporting
+echo "Removing error reporting..."
+sudo apt remove -y whoopsie apport apport-gtk ubuntu-report
+
 # Uninstall unused dependencies
-sudo apt autoremove
+echo "Removing unused dependencies..."
+sudo apt autoclean autoremove
+
+echo "Reboot for all changes to take effect."
