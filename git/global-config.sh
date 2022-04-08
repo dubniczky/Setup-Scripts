17dd5557ce794bd213c8b70e6cf79ddeaca37b10
Richard Nagy <nagy.richard.antal@gmail.com>
2022-04-08 13:28:17 +0000

    Added lfs installer to git global config

--- git/global-config.sh
+++ git/global-config.sh
@@ -1,6 +1,12 @@
+# Set globals
 git config --global user.name "Richard Nagy"
 git config --global user.email "nagy.richard.antal@gmail.com"
 git config --global credential.helper store
 git config --global core.autocrlf true
 git config --global init.defaultbranch main
+
+# Install LFS
+git lfs Install
+
+# List config
 git config --list
