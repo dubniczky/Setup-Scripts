b1f5ab3e00f97984310dc38f98683512ecd0e3d7
Richard Nagy <nagy.richard.antal@gmail.com>
2022-04-13 21:10:22 +0000

    Added Linux docker install script

---
+++ linux/install-docker.sh
@@ -0,0 +1,31 @@
+# Remove old versions (legacy container)
+sudo apt remove \
+    docker \
+    docker.io \
+    containerd \
+    runc
+
+# Install required tools
+sudo apt install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+# Install Docker GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
+    | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+# Set up stable repository
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+    | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update package list with new keys
+sudo apt update
+
+# Install Docker
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# Test installation
+sudo docker run hello-world
+#sudo docker run -it ubuntu bash
