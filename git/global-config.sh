ba51ce7c1e0055ed40bf2b5790d907b33272d6e2
Richard Nagy <nagy.richard.antal@gmail.com>
2022-04-21 13:26:57 +0000

    Added windows specific gpg path config

--- git/global-config.sh
+++ git/global-config.sh
@@ -8,6 +8,7 @@ git config --global init.defaultbranch main
 # Setup GPG signing
 git config --global user.signingkey 3C0B1B83DB3B010F
 git config --global commit.gpgsign true
+#git config --global gpg.program "C:\Program Files (x86)\gnupg\bin\gpg.exe"
 
 # List config
 git config --list
