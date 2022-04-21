3a442c91df1c43ae9fa48587ea62c04b526199d7
Richard Nagy <nagy.richard.antal@gmail.com>
2022-04-21 13:25:08 +0000

    Updated git user name

--- git/global-config.sh
+++ git/global-config.sh
@@ -1,5 +1,5 @@
 # Set globals
-git config --global user.name "Richard Nagy"
+git config --global user.name "Richard Antal Nagy"
 git config --global user.email "nagy.richard.antal@gmail.com"
 git config --global credential.helper store
 git config --global core.autocrlf true
