0bf72c6561247e0997676979baefe37ab2e96180
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-05-25 10:28:44 +0200

    Added zsh install script

---
+++ linux/zsh-setup.sh
@@ -0,0 +1,16 @@
+# Install ZSH
+sudo apt install -y zsh exa
+zsh --version
+
+# Set as default terminal
+chsh -s $(which zsh)
+
+# Install oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install autosuggestions plugin
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Back up and replace .zshrc config
+mv ~/.zshrc ~/.zshrc.bkp
+cp ./.zshrc ~/.zshrc
