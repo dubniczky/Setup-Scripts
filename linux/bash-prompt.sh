9f0faa247067d78aca087036e16e7e7844f07231
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-07-05 23:02:55 +0200

    Updated bash prompt format

--- linux/bash-prompt.sh
+++ linux/bash-prompt.sh
@@ -1,2 +1,6 @@
-# Update command line prompt
+## Author: Richard Antal Nagy
+## Repository: https://gitlab.com/richardnagy/system-setup-scripts
+## Installs a custom bash prompt to .bashrc config file
+
+## Update command line prompt
 cat ./bash-prompt/ps1.sh >> ~/.bashrc
