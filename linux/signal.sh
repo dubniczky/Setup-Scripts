3a918c497e100e45e13523600258dc9e2039f71a
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-07-07 12:37:49 +0200

    Updated signal install script

---
+++ linux/signal.sh
@@ -0,0 +1,15 @@
+## Author: Richard Antal Nagy
+## Repository: https://gitlab.com/richardnagy/system-setup-scripts
+## Installs signal messenger on debian based systems
+
+## Install official Signal public software signing key
+wget -O- https://updates.signal.org/desktop/apt/keys.asc \
+    | gpg --dearmor >> /usr/share/keyrings/signal-desktop-keyring.gpg
+
+# Add Signal repository
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' \
+    | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+
+# Update package database and install signal
+sudo apt update
+sudo apt install -y signal-desktop
