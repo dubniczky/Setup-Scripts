2d464908dadbc8884399ec2a1868392629d961ea
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-07-07 12:06:13 +0200

    Updated mongodb script

--- linux/mongodb.sh
+++ linux/mongodb.sh
@@ -2,21 +2,23 @@
 ## Repository: https://gitlab.com/richardnagy/system-setup-scripts
 ## Installs MongoDB database and shell on a debian based system
 
-# Update PM
-sudo apt-get update
+## Settings
+MONGODB_VERSION="5.0"
 
-# Download PM keys
-wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list
+## Download PM keys
+wget -qO - https://www.mongodb.org/static/pgp/server-$MONGODB_VERSION.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/$MONGODB_VERSION multiverse" \
+    | sudo tee /etc/apt/sources.list.d/mongodb-org-$MONGODB_VERSION.list
 
-# Instalal packages
-sudo apt-get install -y mongodb-org
-sudo apt-get install -y mongodb-mongosh
+## Install packages
+sudo apt install -y \
+    mongodb-org \
+    mongodb-mongosh
 
-# Add service
+## Add as a service
 sudo curl -o /etc/init.d/mongodb https://raw.githubusercontent.com/mongodb/mongo/master/debian/init.d
 sudo chmod +x /etc/init.d/mongodb
 sudo service mongodb enable
 
-# Start service
+## Start service
 sudo service mongodb start
