092ab51bfea282d88123236d99ab469a47a9ffad
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-08-04 12:55:35 +0200

    Updated redis script

--- redis.sh
+++ redis.sh
@@ -2,39 +2,12 @@
 
 ## Author: Richard Antal Nagy
 ## Repository: https://gitlab.com/richardnagy/system-setup-scripts
-## Install redis and GUI package
+## Install Redis and Redis Commander packages
+
+NPM="pnpm"
 
 ## Install Redis
-sudo apt-add-repository ppa:redislabs/redis
-sudo apt update -y
 sudo apt install -y redis-server
 
-## Start Redis service
-sudo service redis-server restart
-
-## Install Ruby for Redis GUI
-# https://github.com/humante/redis-browser
-sudo apt install -y ruby-full
-
-## Install redis gui gem
-sudo gem install redis-browser
-
-sudo tee /etc/init.d/redis-browser.service &> /dev/null << EOF!
-[Unit]
-Description=Browser Interface for Redis
-After=network.target
-StartLimitIntervalSec=0
-
-[Service]
-Type=simple
-Restart=always
-RestartSec=1
-User=centos
-ExecStart=redis-browser --url redis://127.0.0.1:6379
-
-[Install]
-WantedBy=multi-user.target
-EOF!
-
-##
-redis-browser --url redis://127.0.0.1:6379
+## Install redis commander
+$NPM install -g redis-commander
